c6e8fd54fb56c685d9f07032741c5eaf02dfde1b
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-20 07:51:26 +0100

    Create frame.java

---
+++ src/main/java/me/Minionguyjpro/Simple-Java-Game/frame.java
@@ -0,0 +1,7 @@
+JFrame frame = new JFrame("Title");                 
+frame.add(game);                                      
+frame.pack();                                         
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+frame.setResizable(false);                              
+frame.setLocationRelativeTo(null);                     
+frame.setVisible(true);
