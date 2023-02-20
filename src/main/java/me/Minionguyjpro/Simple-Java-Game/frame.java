0bc205c09ff222202d1049f33ed8f90d5b4bf7bb
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-20 11:53:49 +0100

    Update frame.java

--- src/main/java/me/Minionguyjpro/Simple-Java-Game/frame.java
+++ src/main/java/me/Minionguyjpro/Simple-Java-Game/frame.java
@@ -1,3 +1,4 @@
+{
 JFrame frame = new JFrame("Title");                 
 frame.add(game);                                      
 frame.pack();                                         
@@ -5,3 +6,4 @@ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setResizable(false);                              
 frame.setLocationRelativeTo(null);                     
 frame.setVisible(true);
+}
