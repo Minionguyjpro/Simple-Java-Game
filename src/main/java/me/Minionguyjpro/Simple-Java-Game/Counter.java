241e9d9d98bfd2e773b71ce39e7cfe7edb9e5272
Minionguyjpro <66115754+Minionguyjpro@users.noreply.github.com>
2023-02-20 08:01:24 +0100

    Create Counter.java

---
+++ src/main/java/me/Minionguyjpro/Simple-Java-Game/Counter.java
@@ -0,0 +1,12 @@
+import java.lang.*;
+// Inherit Thread
+public class Counter extends Thread      
+{
+        // Declare a new Thread
+        Thread T; 
+        // The order in which the program should be executed                           
+        public void run()                     
+        {                              
+        ....            
+        }
+}
